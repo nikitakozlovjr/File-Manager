cef1cec30a93a8c4e4fcd1a3507d888213745355
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-28 00:50:41 +0300

    Добавили шебанг

---
+++ bin/getDirectorySize.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import fsp from 'fs/promises'
+import async from 'async';
+import path from 'path';
+
+const getDirectirySize = ()
+
+console.log(getDirectirySize('../__fixtures__').then((content) => console.log(content)));
