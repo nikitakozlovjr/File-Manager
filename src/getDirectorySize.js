ddf6e20062fda249106f350a4cab29a9d307bab7
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-28 00:43:37 +0300

    Произвели исправления

---
+++ src/getDirectorySize.js
@@ -0,0 +1,7 @@
+import fsp from 'fs/promises'
+import async from 'async';
+import path from 'path';
+
+const getDirectirySize = ()
+
+console.log(getDirectirySize('../__fixtures__').then((content) => console.log(content)));
