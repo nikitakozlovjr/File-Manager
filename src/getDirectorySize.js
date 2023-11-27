dc074becb9f5ac3e95f4b2cd3e53525800a4772f
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-28 01:17:39 +0300

    Переместили файлы в директорию bin

--- src/getDirectorySize.js
+++
@@ -1,7 +0,0 @@
-import fsp from 'fs/promises'
-import async from 'async';
-import path from 'path';
-
-const getDirectirySize = ()
-
-console.log(getDirectirySize('../__fixtures__').then((content) => console.log(content)));
