a19251a09e6aefbb20c3cc2366ba29fa58215b9b
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-28 03:31:46 +0300

    Добавили в исполняемый файл получение проверки

--- bin/getDirectorySize.js
+++ bin/getDirectorySize.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-import getDirectorySize from 'getDirectorySize';
+import path from 'path';
+import getDirectorySize from '../src/getDirectorySize.js';
+
+const dirname = process.argv[2]; 
+
+const buldpath = (dirname) => {
+    if(typeof dirname !== 'string') {
+        throw new Error('You did not provide the directory path');
+    };
+
+    return path.resolve(process.cwd(), dirname);
+}
+
+const dirpath = buldpath(dirname);
+
+getDirectorySize(dirpath);
