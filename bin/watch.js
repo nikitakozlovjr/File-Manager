295fcef80221f400b294c554c6986666283a8b0f
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-29 02:59:40 +0300

    Написали quardExpression для получения корректных данных

--- bin/watch.js
+++ bin/watch.js
@@ -1,1 +1,19 @@
-console.log('Wath!')
+#!/usr/bin/env node
+
+import path from 'path';
+import watch from '../src/watch.js';
+
+const filepath = process.argv[2]; 
+const interval = process.argv[3];
+const timeWork = process.argv[4];
+
+if(typeof filepath !== 'string') {
+    throw new Error(`Incorrectly entered path to the file, the path to the file is expected as a string, but is received ${typeof filepath}`);
+}
+if(isNaN(Number(interval))) {
+    throw new Error(`Incorrectly entered interval. Number expected as input`);
+}
+if(isNaN(Number(timeWork))) {
+    throw new Error(`Incorrectly entered working time of the module. Number expected as input`);
+}
+
