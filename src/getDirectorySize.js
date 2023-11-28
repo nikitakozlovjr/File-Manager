d86df2a376b4bae92fa22eb7a007c2ad57159264
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-29 02:39:08 +0300

    Работа с фикстурами

--- src/getDirectorySize.js
+++ src/getDirectorySize.js
@@ -16,5 +16,3 @@ export default (dirpath) => {
     getDirectorySize(dirpath).then((result) => console.log(result))
                              .catch((err) => console.log(`Error!\n${err}`));
 }
-
-// getDirectorySize('../__fixtures__').then((result) => console.log(result));
