8ea79d95abd6c6175f03274cd7ad600892538eda
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-29 03:18:53 +0300

    Дописали исполняемый файл

--- bin/watch.js
+++ bin/watch.js
@@ -3,17 +3,24 @@
 import path from 'path';
 import watch from '../src/watch.js';
 
-const filepath = process.argv[2]; 
-const interval = process.argv[3];
-const timeWork = process.argv[4];
+const buildpath = (filepath) => path.resolve(process.cwd(), filepath);
 
-if(typeof filepath !== 'string') {
+const firstArgument = process.argv[2]; 
+const secondArgument = process.argv[3];
+const thirdArgument = process.argv[4];
+
+if(typeof firstArgument !== 'string') {
     throw new Error(`Incorrectly entered path to the file, the path to the file is expected as a string, but is received ${typeof filepath}`);
 }
-if(isNaN(Number(interval))) {
+if(isNaN(Number(secondArgument))) {
     throw new Error(`Incorrectly entered interval. Number expected as input`);
 }
-if(isNaN(Number(timeWork))) {
+if(isNaN(Number(thirdArgument))) {
     throw new Error(`Incorrectly entered working time of the module. Number expected as input`);
 }
 
+const filepath = buildpath(firstArgument);
+const interval = Number(secondArgument);
+const timeWork = Number(thirdArgument);
+
+watch(filepath, interval, timeWork);
