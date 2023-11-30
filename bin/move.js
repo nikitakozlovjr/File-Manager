e60f8f22e365e9736836aa52d2af471e10d3be72
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-30 22:57:35 +0300

    Сделали файлы исполняемыми

--- bin/move.js
+++ bin/move.js
@@ -1,17 +1,1 @@
-import fsp from 'fs/promises';
-import path from 'path';
-
-const move = (outfile, intofile) => {
-    const [outfilepath, intofilepath] = [outfile, intofile]
-        .map((file) => path.join('../__fixtures__', file));
-    
-    return fsp.readFile(outfilepath, 'utf-8')
-        .then((data) => fsp.writeFile(intofilepath, data)
-            .then(() => fsp.unlink(outfilepath)
-                .then(() => console.log(`Sucess!\nNote that the ${outfilepath} has now been deleted`))
-                .catch(() => `Error in deleting file, file ${outfilepath} overwritten in ${intofilepath}, but not deleted`))
-            .catch((e) => console.log('Error im writing!', e)))
-        .catch((e) => console.log('Error in reading!\n', e));
-};
-
-export default move;
+#!/usr/bin/env node
