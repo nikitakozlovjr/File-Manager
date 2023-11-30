659b045992f3a10fe92652eac8c1ae507f9982ae
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-30 23:23:35 +0300

    Импортировали заисимости в исполняемый модуль

--- bin/print.js
+++ bin/print.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+import print from '../src/print.js';
+import path from 'path';
+
+const filename = process.argv[2];
+
+const buldpath = (filename) => path.resolve(process.cwd(), filename);
+
+print(buldpath(filename));
