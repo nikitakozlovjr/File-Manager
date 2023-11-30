bdbb004203291a8766d8c5ad3d0b5371493b3b31
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-30 23:22:32 +0300

    Прописали логику работы функции

--- src/print.js
+++ src/print.js
@@ -1,8 +1,6 @@
 import fsp from 'fs/promises';
-import path from 'path';
 
-const print = (file) => {
-    const filepath = path.join('../__fixtures__', file);
+const print = (filepath) => {
     fsp.readFile(filepath, 'utf-8').then((data) => console.log(data))
                                    .catch((e) => console.log('Erorr!\n', e)) 
 }
