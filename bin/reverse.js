7c04d8c34f081c8abc2c42e8e4ec07d78ed4348d
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-03 00:26:09 +0300

    Написали исполняемый файл

--- bin/reverse.js
+++ bin/reverse.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+import reverse from '../src/reverse.js';
+import path from 'path';
+
+const bulidpath = (filename) => path.resolve(process.cwd(), filename);
+
+const filename = process.argv[2];
+
+reverse(bulidpath(filename));
