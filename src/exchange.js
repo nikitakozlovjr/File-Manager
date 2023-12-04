6aa91292889ea9e80c012d3834273187d2728193
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-05 02:02:33 +0300

    Написали логику функции

---
+++ src/exchange.js
@@ -0,0 +1,18 @@
+import fsp from 'fs/promises';
+import async from 'async';
+
+const exchange = async (filepaths) => {
+    const promises = filepaths.map((filepath) => fsp.readFile(filepath, 'utf-8'));
+    const [data1, data2] = await Promise.all(promises);
+
+    const writing1 = fsp.writeFile(filepaths[0], data2);
+    const writing2 = fsp.writeFile(filepaths[1], data1);
+
+    return await Promise.all([writing1, writing2]);
+};
+
+export default (...filepaths) => {
+    exchange(filepaths)
+        .then(() => console.log('Succes!'))
+        .catch((e) => console.log(`Error!\n${e}`));
+}
