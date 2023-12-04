a9c87eac3afe6f50fb11493d5807192905238710
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-05 00:40:26 +0300

    правки отступов

--- src/getTypes.js
+++ src/getTypes.js
@@ -3,13 +3,14 @@ import async from 'async';
 
 const getTypes = async (paths) => {
     const promises = paths.map((path) => fsp.stat(path)
-        .then((stat) => stat.isDirectory())
+        .then((stat) => stat.isDirectory() ? 'directoty' : 'file')
         .catch(() => null));
 
     const stats = await Promise.all(promises);
     
     return stats;
 };
+
 
 export default (paths) => {
     getTypes(paths).then((result) => console.log(result));
