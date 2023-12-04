1bdc92a96002b6469d998483092c26f75a44696e
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-05 00:39:54 +0300

    Напсиалси исполняемый модуль

--- bin/getTypes.js
+++ bin/getTypes.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+import getTypes from '../src/getTypes.js';
+import path from 'path';
+
+const paths = process.argv.slice(2);
+
+const buildpath = (filename) => path.resolve(process.cwd(), filename);
+
+const absPaths = paths.map((path) => buildpath(path));
+
+getTypes(absPaths);
