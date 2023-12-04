1aa1ee70ffaddbfe6677e85214c3908cb1e9b74d
nikitakozlovjr <nikita3726898@gmail.com>
2023-12-05 02:11:43 +0300

    Написали исполняемый модуль

--- bin/exchange.js
+++ bin/exchange.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Boom!')
+import exchange from '../src/exchange.js';
+import path from 'path';
+
+const buildpath = (filename) => path.resolve(process.cwd(), filename);
+
+const filenames = process.argv.slice(2);
+
+const absPath = filenames.map((filename) => buildpath(filename));
+
+exchange(absPath);
